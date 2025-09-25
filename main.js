f89db4b7c74934a07b64612ca4ab23d3c8f89565
Diego Noriega <Diego.ale036@gmail.com>
2025-09-25 12:54:19 -0500

    Adding event listener for calculate button, fixing some problems with the text input, and two blank functions for calculation

--- main.js
+++ main.js
@@ -6,3 +6,34 @@ inputParameters.forEach(function(input) {
     });
 });
 
+const calcButton = document.querySelector('#calculate-btn');
+const amountInput = document.getElementById('input-amount-box');
+const termInput = document.getElementById('input-term');
+const rateInput = document.getElementById('input-rate')
+
+calcButton.addEventListener('click', e => {
+    e.preventDefault();
+});
+
+
+const repayType = document.querySelector('#repayment-btn');
+const interestType = document.querySelector('#interest-only-btn');
+
+function calcFunction() {
+    const resultsDefault = document.querySelector('.results-default').style.display = 'none';
+    const resultsActive = document.querySelector('.results-active').style.display = 'flex';
+    
+    if (repayType.checked === true) {
+        return repayCalc();
+    } 
+    else if (interestType.checked === true){
+        return interCalc();
+    } 
+    else {
+        return false;
+    }
+};
+
+function repayCalc(a, b) {}
+
+function interCalc(a, b) {}
