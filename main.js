49d52b45089279286ba50d2ee9b8a39d66ff4962
Diego Noriega <Diego.ale036@gmail.com>
2025-09-25 15:27:28 -0500

    Validation input added (probably need to change some things in the validation)

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+    // Dollar Formatter
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
+
+    // Input Parameter and Validation Function
 const inputParameters = document.querySelectorAll('.input');
 
 inputParameters.forEach(function(input) {
@@ -9,17 +16,58 @@ inputParameters.forEach(function(input) {
 const calcButton = document.querySelector('#calculate-btn');
 const amountInput = document.getElementById('input-amount-box');
 const termInput = document.getElementById('input-term');
-const rateInput = document.getElementById('input-rate')
+const rateInput = document.getElementById('input-rate');
 
 calcButton.addEventListener('click', e => {
     e.preventDefault();
+    validFunc();
 });
-
 
 const repayType = document.querySelector('#repayment-btn');
 const interestType = document.querySelector('#interest-only-btn');
 
-function calcFunction() {
+function validFunc() {
+    const amountError = document.querySelector('#amount-err-msg');
+    const termError = document.querySelector('#term-err-msg');
+    const rateError = document.querySelector('#rate-err-msg');
+    const typeError = document.querySelector('#type-err-msg');
+
+    let isValid = true;
+    
+    if (amountInput.value === '') {
+        amountError.style.visibility = "visible";
+        isValid = false;
+    } else {
+        amountError.style.visibility = "hidden";
+    }
+
+    if (termInput.value === '') {
+        termError.style.visibility = "visible"
+        isValid = false;
+    } else {
+        termError.style.visibility = "hidden";
+    }
+
+    if (rateInput.value === '') {
+        rateError.style.visibility = "visible"
+        isValid = false;
+    } else {
+        rateError.style.visibility = "hidden"
+    }
+
+    if (repayType.checked !== true && interestType.checked !== true) {
+        typeError.style.visibility = "visible"
+        isValid = false;
+    } else {
+        typeError.style.visibility = "hidden"
+    }
+
+    if (isValid === true) {
+        return isValid && calc();
+    }
+};
+
+function calc() {
     const resultsDefault = document.querySelector('.results-default').style.display = 'none';
     const resultsActive = document.querySelector('.results-active').style.display = 'flex';
     
@@ -34,6 +82,6 @@ function calcFunction() {
     }
 };
 
-function repayCalc(a, b) {}
+function repayCalc(amount, rate, term) {}
 
-function interCalc(a, b) {}
+function interCalc(amount, rate) {}
